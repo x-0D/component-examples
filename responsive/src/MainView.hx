9c81d18554e582c36091dfdfb8b1ee9c6321cc7c
Nikita Bragin <87000516+x-0D@users.noreply.github.com>
2024-10-20 15:09:50 +0000

    add responsive ui example

---
+++ responsive/src/MainView.hx
@@ -0,0 +1,10 @@
+package ;
+
+import haxe.ui.containers.ScrollView;
+
+@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
+class MainView extends ScrollView {
+    public function new() {
+        super();
+    }
+}
